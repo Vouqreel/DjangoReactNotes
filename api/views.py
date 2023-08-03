0902474991af46c4fb07a7ab14487fa31a0f8bc1
Vazgen <vouqreel@gmail.com>
2023-08-03 22:00:09 +0500

    configuration for the django admin panel

--- api/views.py
+++ api/views.py
@@ -1,11 +1,16 @@
 from django.shortcuts import render
-from django.http import JsonResponse
+from rest_framework.response import Response
+from rest_framework.decorators import api_view
+from .models import Note
+from .serializers import NoteSerializer
+
+
 
 # Create your views here.
 
-
+@api_view(['GET'])
 def getRoutes(request):
-   routes = [
+   routes = [ 
       {
          'Endpoint': '/notes/',
          'method': 'GET',
@@ -37,5 +42,11 @@ def getRoutes(request):
          'description': 'Deletes and exiting note'
       },
    ]
-   
-   return JsonResponse(routes, safe=False)
+
+   return Response(routes )   
+
+@api_view(['GET'])
+def getNotes(request):
+   notes = Note.objects.all()
+   serializers = NoteSerializer(notes, many=True)
+   return Response(serializers.data) 
